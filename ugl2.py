93db600f13e97cad3d2febc0104dd9bc3a967396
gbellino1 <gabriel.bellino@gmail.com>
2026-07-06 09:52:38 -0300

    Update ugl2.py

--- ugl2.py
+++ ugl2.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import datetime
 import pandas as pd
 
